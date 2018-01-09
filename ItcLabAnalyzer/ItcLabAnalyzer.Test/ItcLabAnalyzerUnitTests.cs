cd7b17723db4515e1f21b653f40d5eb9d9696504
Maciej Gamrat <maciej@gamrat.it>
2018-01-09 01:15:43 +0100

    Remove example tests

--- ItcLabAnalyzer/ItcLabAnalyzer.Test/ItcLabAnalyzerUnitTests.cs
+++ ItcLabAnalyzer/ItcLabAnalyzer.Test/ItcLabAnalyzerUnitTests.cs
@@ -21,54 +21,6 @@ namespace ItcLabAnalyzer.Test
             VerifyCSharpDiagnostic(test);
         }
 
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public void TestMethod2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
-        }
-    }";
-            var expected = new DiagnosticResult
-            {
-                Id = "ItcLabAnalyzer",
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
-                Locations =
-                    new[] {
-                            new DiagnosticResultLocation("Test0.cs", 11, 15)
-                        }
-            };
-
-            VerifyCSharpDiagnostic(test, expected);
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-            VerifyCSharpFix(test, fixtest);
-        }
-
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
             return new ItcLabAnalyzerCodeFixProvider();
