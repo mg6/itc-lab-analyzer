aeb118f4c9956c76a4286c7e0896ad9614903064
Maciej Gamrat <maciej@gamrat.it>
2018-01-11 02:41:35 +0100

    Move path combine analyzer to separate class

--- ItcLabAnalyzer/ItcLabAnalyzer/ItcLabAnalyzerAnalyzer.cs
+++ ItcLabAnalyzer/ItcLabAnalyzer/ItcLabAnalyzerAnalyzer.cs
@@ -14,33 +14,22 @@ namespace ItcLabAnalyzer
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class ItcLabAnalyzerAnalyzer : DiagnosticAnalyzer
     {
+        private PathCombineAnalyzer CombineAnalyzer = new PathCombineAnalyzer();
+
         public const string DiagnosticId = "ItcLabAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
-
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
-
-        public override void Initialize(AnalysisContext context)
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
         {
-            context.RegisterSyntaxNodeAction(AnalyzeStringPath, SyntaxKind.StringLiteralExpression);
-        }
-
-        private void AnalyzeStringPath(SyntaxNodeAnalysisContext context)
-        {
-            var checkString = (LiteralExpressionSyntax)context.Node;
-            if (Regex.Match(checkString.Token.Text, "\\\\").Success || (checkString.Token.Text.Contains("@\"") && checkString.Token.Text.Contains('\\')))
+            get
             {
-                var diagnostic = Diagnostic.Create(Rule, checkString.GetLocation(), checkString);
-                context.ReportDiagnostic(diagnostic);
+                return ImmutableArray.Create(
+                    PathCombineAnalyzer.Rule);
             }
         }
 
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(CombineAnalyzer.AnalyzeStringPath, SyntaxKind.StringLiteralExpression);
+        }
     }
 }
