9ac4c0f04a3026b75ab64084b2fccedd7cb1ad86
Maciej Gamrat <maciej@gamrat.it>
2018-01-12 02:32:19 +0100

    Implement missing dispose analyzer

--- ItcLabAnalyzer/ItcLabAnalyzer/DisposablesAnalyzer.cs
+++ ItcLabAnalyzer/ItcLabAnalyzer/DisposablesAnalyzer.cs
@@ -15,16 +15,36 @@ namespace ItcLabAnalyzer
     {
         public const string DiagnosticId = "DisposablesAnalyzer";
 
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.DisposablesAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.DisposablesAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.DisposablesAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        public static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.DisposablesAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        public static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.DisposablesAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        public static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.DisposablesAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
         public static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
         public void Analyze(SyntaxNodeAnalysisContext context)
         {
-            throw new NotImplementedException();
+            var expression = context.Node as ObjectCreationExpressionSyntax;
+
+            var declarator = expression.Parent.Parent as VariableDeclaratorSyntax;
+            var identifier = declarator.Identifier;
+
+            var classType = context.SemanticModel.GetTypeInfo(expression).ConvertedType;
+            var implementedInterfaces = classType.AllInterfaces;
+
+            if (implementedInterfaces.Select(e => e.Name).Contains(nameof(IDisposable)))
+            {
+                var root = context.SemanticModel.SyntaxTree.GetRoot(context.CancellationToken);
+                var disposeInvocations = root.DescendantNodes()
+                    .OfType<MemberAccessExpressionSyntax>()
+                    .Where(e => (e.Expression as IdentifierNameSyntax).Identifier.Text == identifier.Text);
+
+                if (!disposeInvocations.Any())
+                {
+                    var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation(), context.Node);
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
         }
     }
 }
