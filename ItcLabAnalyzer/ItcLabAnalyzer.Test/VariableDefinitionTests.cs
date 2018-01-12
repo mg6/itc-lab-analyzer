a8effadf19ee1dd825d773f4c6d1dfe488dbbc4b
Maciej Gamrat <maciej@gamrat.it>
2018-01-12 01:10:08 +0100

    Rename/expand on long variable code fix tests

--- ItcLabAnalyzer/ItcLabAnalyzer.Test/VariableDefinitionTests.cs
+++ ItcLabAnalyzer/ItcLabAnalyzer.Test/VariableDefinitionTests.cs
@@ -12,7 +12,7 @@ namespace ItcLabAnalyzer.Test
     public class VariableDefinitionTests : CodeFixVerifier
     {
         [TestMethod]
-        public void TestNameWithUpperLetters()
+        public void TestPascalCaseVariableName()
         {
             var test = @"
     using System;
@@ -57,7 +57,7 @@ namespace ItcLabAnalyzer.Test
         }
 
         [TestMethod]
-        public void TestNameWithUnderScore()
+        public void TestSnakeCaseVariableName()
         {
             var test = @"
     using System;
@@ -102,7 +102,53 @@ namespace ItcLabAnalyzer.Test
         }
 
         [TestMethod]
-        public void TestNameWithUpperLettersAndNumbers()
+        public void TestSnakeCaseVariableNameWithNumbers()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+    using System.IO;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bardzo_bardzo_22_długa_nazwa_to_jest_33;
+                int bardzo_bardzo_44długa_nazwa_to_jest55;
+            }
+        }
+    }
+";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+    using System.IO;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bbdntj;
+            }
+        }
+    }
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void TestPascalCaseVariableNameWithNumbers()
         {
             var test = @"
     using System;
@@ -119,6 +165,7 @@ namespace ItcLabAnalyzer.Test
             void Method() 
             {
                 int BardzoBardzoDługaNazwaToJest33;
+                int BardzoBardzo55DługaNazwaToJest;
             }
         }
     }
@@ -139,6 +186,7 @@ namespace ItcLabAnalyzer.Test
             void Method() 
             {
                 int bbdntj33;
+                int bb55dntj;
             }
         }
     }
@@ -147,7 +195,7 @@ namespace ItcLabAnalyzer.Test
         }
 
         [TestMethod]
-        public void TestNameWithLowerLetters()
+        public void TestLowerCaseVariableName()
         {
             var test = @"
     using System;
@@ -191,6 +239,141 @@ namespace ItcLabAnalyzer.Test
             VerifyCSharpFix(test, fixtest);
         }
 
+        [TestMethod]
+        public void TestManySnakeCaseVariableNames()
+        {
+            var test = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bardzo_bardzo_długa_nazwa_to_jest,
+                    inna_bardzo_bardzo_długa_nazwa_to_jest;
+            }
+        }
+    }
+";
+
+            var fixtest = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bbdntj,
+                    ibbdntj;
+            }
+        }
+    }
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void TestManyPascalCaseVariableNames()
+        {
+            var test = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int BardzoBardzoDługaNazwaToJest,
+                    InnaBardzoBardzoDługaNazwaToJest;
+            }
+        }
+    }
+";
+
+            var fixtest = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bbdntj,
+                    ibbdntj;
+            }
+        }
+    }
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void TestPascalCaseVariableNameWithInitializer()
+        {
+            var test = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int BardzoBardzoDługaNazwaToJest = 5 + 6;
+            }
+        }
+    }
+";
+
+            var fixtest = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bbdntj = 5 + 6;
+            }
+        }
+    }
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [TestMethod]
+        public void TestSnakeCaseVariableNameWithInitializer()
+        {
+            var test = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bardzo_bardzo_długa_nazwa_to_jest = 5 + 6;
+            }
+        }
+    }
+";
+
+            var fixtest = @"
+    using System;
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {           
+            void Method() 
+            {
+                int bbdntj = 5 + 6;
+            }
+        }
+    }
+";
+            VerifyCSharpFix(test, fixtest);
+        }
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
